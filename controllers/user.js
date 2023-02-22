cf392b00278c2c6ffc51d2049c15c859e39fcf30
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-02-22 19:42:07 +0100

    email sending with nodemailer

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const _ = require("lodash");
 const config = require("config");
 const secretOrkey = config.get("secretOrkey");
+const nodemailer = require("nodemailer");
 
 //Password Crypt
 const bcrypt = require("bcryptjs");
@@ -18,6 +19,36 @@ exports.register = async (req, res) => {
       return res
         .status(401)
         .json({ msg: `Utilisateur existant , utiliser un autre E-mail` });
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "zaghouani.yosri@gmail.com", // generated ethereal user
+        pass: "yimktgkvxvbbylzp", // generated ethereal password
+      },
+      tls: { rejectUnothorized: false },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Node mailer contact" <zaghouani.yosri@gmail.com>', // sender address
+      to: email, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    console.log("email has been sent");
 
     const newUser = new User({
       name,
