cd3bff4da1e99037f2077223e64866d4ec92119a
KvRae <karamelmannai@gmail.com>
2023-04-01 02:05:39 +0100

    optimizing node dep + improving mailing scheduler performance

--- utils/mailingScheduler.js
+++ utils/mailingScheduler.js
@@ -1,46 +1,54 @@
 const schedule = require('node-schedule');
 const User = require('../models/User');
+const Email = require('../models/email');
 const paymentController = require('../controllers/paymentsMailing');
 const today = new Date().getTime();
 const userController = require('../controllers/user')
 const mailingService = require('../controllers/emailServiceController')
 
-const mailingExpire = schedule.scheduleJob('0 0 * * *', function(){
-    User.find().then(users => {
-        users.forEach(user => {
-            if(user.subscription !== "Blocked" && user.endedAt.getTime() < today){
-                paymentController.sendEmailAfterExpirationDate(user.email,user.name)
-                userController.blockUser(user.email)
-
+// mailingExpire
+const mailingExpire = schedule.scheduleJob('0 0 * * *', async function(){
+    const users = await User.find();
+    for (const user of users) {
+        if(user.subscription !== "Blocked" && user.endedAt.getTime() < today){
+            await paymentController.sendEmailAfterExpirationDate(user.email, user.name);
+            await userController.blockUser(user.email);
+        }
+    }
+});
+// subscriptionReminderJob
+const subscriptionReminderJob = schedule.scheduleJob('0 0 * * *', async function(){
+    try {
+        const users = await User.find();
+        for (const user of users) {
+            const days = parseInt((user.endedAt - new Date()) / (1000 * 60 * 60 * 24), 10);
+            if(days === 7 && user.subscription !== "Blocked"){
+                await paymentController.sendEmailBeforeExpirationDate(user.email, user.name);
             }
-        })
-    })
-  });
+        }
+    } catch (error) {
+        console.error('Error occurred while sending subscription reminder emails:', error);
+    }
+});
 
+// mailingServiceJob
+const mailingServiceJob = schedule.scheduleJob("*/1 * * * *", async function() {
+    try {
+        const now = new Date().getTime();
+        const emails = await Email.find({ scheduleTime: { $lt: now } })
+            .sort({ scheduleTime: 'asc' })
+            .limit(100);
 
-  const mailingBeforeDays = schedule.scheduleJob('0 0 * * *', function(){
-    User.find().then(users => {
-        users.forEach(user => {
-            const days = parseInt((user.endedAt - new Date()) / (1000 * 60 * 60 * 24), 10); 
-            if(days === 7 && user.subscription !=="Blocked"){
-                paymentController.sendEmailBeforeExpirationDate(user.email,user.name)
-            }
-        })
-    })
-  });
+        for (const email of emails) {
+            await mailingService.sendScheduledEmail(email._id);
+            await Email.findByIdAndDelete(email._id);
+            console.log(`Sent scheduled email ${email._id}`);
+        }
 
-  // mailingServiceJob
- const mailingServiceJob = schedule.scheduleJob('* */1 * * * *', function(){
-
-
- });
-
-  // const time = 10
-  // var i = 0
-  // const pushNotification = schedule.scheduleJob('*/1 * * * * *', function(){
-  //   i++
-  //     console.log(i)
-  //   if (time === i) {
-  //     console.log('push notification')}
-  // });
-
+        if (emails.length > 0) {
+            console.log(`Sent ${emails.length} scheduled emails`);
+        }
+    } catch (error) {
+        console.error('Error occurred while sending scheduled emails:', error);
+    }
+});
