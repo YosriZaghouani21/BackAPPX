195c51258d43ba981d9c9bc4d95562857af60fde
KvRae <karamelmannai@gmail.com>
2023-04-23 13:13:29 +0100

    Customizing apis

--- bin/bat/api-generator-push.bat
+++ bin/bat/api-generator-push.bat
@@ -3,5 +3,7 @@ REM Initialize Git in your project directory (if you haven't already)
 git init
 REM Add your files to the staging area
 git branch -M main
+REM Gitingore
+copy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\.gitignore .gitignore
 REM Add your changes to the staging area
 git add .
---
+++ bin/bat/payment-api-generator.bat
@@ -0,0 +1,10 @@
+REM Files
+copy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\api_types\payment_api\index.js index.js
+copy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\api_types\payment_api\package.json package.json
+copy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\api_types\payment_api\README.md README.md
+copy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\api_types\payment_api\.env .env
+REM Folders
+xcopy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\middleware middleware /E /I /Y
+xcopy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\models models /E /I /Y
+xcopy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\routes routes /E /I /Y
+xcopy C:\Users\karam\Documents\GitHub\BackAPPX\api-generator\common\utils utils /E /I /Y
